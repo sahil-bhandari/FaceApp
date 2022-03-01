394956e40ed106c8fcc1503ccea90fb87da25f94
Sahil <43530216+sahil-bhandari@users.noreply.github.com>
2022-03-01 22:37:14 +0530

    Initial commit

---
+++ app/src/main/java/com/sahil/arface/GlassesActivity.kt
@@ -0,0 +1,171 @@
+package com.sahil.arface
+
+import android.app.ActivityManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.view.PixelCopy
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.AugmentedFace
+import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.ArSceneView
+import com.google.ar.sceneform.Scene
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.rendering.Texture
+import com.google.ar.sceneform.ux.AugmentedFaceNode
+import kotlinx.android.synthetic.main.activity_glasses.*
+
+
+class GlassesActivity : AppCompatActivity() {
+
+    companion object {
+        const val MIN_OPENGL_VERSION = 3.0
+    }
+
+    lateinit var scene: Scene
+    lateinit var arFragment: FaceArFragment
+    private var faceMeshTexture: Texture? = null
+    private var glasses: ArrayList<ModelRenderable> = ArrayList()
+    private var faceRegionsRenderable: ModelRenderable? = null
+    var faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
+    private var index: Int = 0
+    private var changeModel: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Check ARCore and OpenGL ES 3.0 compatibility. Else close.
+        if (!checkIsSupportedDeviceOrFinish()) {
+            return
+        }
+
+        setContentView(R.layout.activity_glasses)
+
+        initViews()
+
+        initAssets()
+
+
+        //Create SceneView
+        val sceneView = arFragment.arSceneView
+        sceneView.cameraStreamRenderPriority = Renderable.RENDER_PRIORITY_FIRST
+        scene = sceneView.scene
+
+        scene.addOnUpdateListener {
+            if (faceRegionsRenderable != null) {
+                sceneView.session?.getAllTrackables(AugmentedFace::class.java)?.let {
+                    for (f in it) {
+                        if (!faceNodeMap.containsKey(f)) {
+                            val faceNode = AugmentedFaceNode(f)
+                            faceNode.setParent(scene)
+                            faceNode.faceRegionsRenderable = faceRegionsRenderable
+                            faceNodeMap[f] = faceNode
+                        } else if (changeModel) {
+                            faceNodeMap.getValue(f).faceRegionsRenderable = faceRegionsRenderable
+                        }
+                    }
+                    changeModel = false
+                    // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
+                    val iter = faceNodeMap.entries.iterator()
+                    while (iter.hasNext()) {
+                        val entry = iter.next()
+                        val face = entry.key
+                        if (face.trackingState == TrackingState.STOPPED) {
+                            val faceNode = entry.value
+                            faceNode.setParent(null)
+                            iter.remove()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initAssets() {
+        Texture.builder()
+            .setSource(this, R.drawable.makeup)
+            .build()
+            .thenAccept { texture -> faceMeshTexture = texture }
+
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("yellow_sunglasses.sfb"))
+            .build()
+            .thenAccept { modelRenderable ->
+                glasses.add(modelRenderable)
+                faceRegionsRenderable = modelRenderable
+                modelRenderable.isShadowCaster = false
+                modelRenderable.isShadowReceiver = false
+            }
+
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("sunglasses.sfb"))
+            .build()
+            .thenAccept { modelRenderable ->
+                glasses.add(modelRenderable)
+                modelRenderable.isShadowCaster = false
+                modelRenderable.isShadowReceiver = false
+            }
+    }
+
+    private fun initViews() {
+
+        ll_preview.visibility=View.GONE
+
+        button_next.setOnClickListener {
+            changeModel = !changeModel
+            index++
+            if (index > glasses.size - 1) {
+                index = 0
+            }
+            faceRegionsRenderable = glasses[index]
+        }
+
+        button_camera.setOnClickListener {
+            val view: ArSceneView = arFragment.arSceneView
+            val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+            PixelCopy.request(view, bitmap, { copyResult ->
+                if (copyResult === PixelCopy.SUCCESS) {
+                    ll_preview.visibility=View.VISIBLE
+                    iv_screen.setImageBitmap(bitmap)
+                } else {
+                    ll_preview.visibility=View.GONE
+                }
+            }, Handler())
+        }
+
+        arFragment = face_fragment as FaceArFragment
+    }
+
+    private fun checkIsSupportedDeviceOrFinish(): Boolean {
+        if (ArCoreApk.getInstance()
+                .checkAvailability(this) == ArCoreApk.Availability.UNSUPPORTED_DEVICE_NOT_CAPABLE
+        ) {
+            Toast.makeText(this, "Augmented Faces requires ARCore", Toast.LENGTH_LONG).show()
+            finish()
+            return false
+        }
+        val openGlVersionString = (getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager)
+            ?.deviceConfigurationInfo
+            ?.glEsVersion
+
+        openGlVersionString?.let {
+            if (java.lang.Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+                Toast.makeText(
+                    this,
+                    "Sceneform requires OpenGL ES 3.0 or later $it",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+                finish()
+                return false
+            }
+        }
+        return true
+    }
+}
